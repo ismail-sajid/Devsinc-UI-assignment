0744cac3043270bebc9d008518ede5063c1c0538
ismail-sajid <ismail.sajid10@gmail.com>
2024-04-25 02:14:07 +0500

    First commit of UI assignment

---
+++ scrpit.js
@@ -0,0 +1,11 @@
+// Custom JavaScript for Interactivity
+
+$(document).ready(function() {
+    // jQuery function to execute after document is fully loaded
+
+    // Event listener for button click
+    $('#changeTextBtn').click(function() {
+        // Change text of the main heading
+        $('h1').text('Welcome to Our Updated Example Website');
+    });
+});
